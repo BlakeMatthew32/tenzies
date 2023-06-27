866ef9d0fc6cac777a10b435336ba956cf2903b3
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2023-06-27 21:24:18 +0100

    Game loop functionality
    added the logic to enable the player to win and also start a new game once they have won.
    Used react confetti to add some visual effects to the win screen.
    added some conditions to change the button text and the onClick function used by the button dependant on if the player has won or not.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Confetti from 'react-confetti'
 import { nanoid } from 'nanoid'
 import './App.css'
 import Dice from './Dice'
@@ -6,18 +7,30 @@ import Dice from './Dice'
 function App() {
 
   const [dice, setDice] = useState(allNewDice())
-  
+  const [tenzies, setTenzies] = useState(false)
 
+  useEffect(() => {
+    const allMatch = dice.every(die => die.isHeld && die.value === dice[0].value)
+    if (allMatch) {
+      setTenzies(allMatch) 
+    }
+  }, [dice])
+
+  function createNewDie() {
+    const randNum = Math.floor(Math.random() * 6 + 1)
+
+    return {
+      value: randNum,
+      isHeld: false,
+      id: nanoid(10)
+    }
+  }
+  
   function allNewDice() {
     const diceValues = []
     for(let i = 0; i < 10; i++) {
-      const randNum = Math.floor(Math.random() * 6 + 1)
         diceValues.push(
-          {
-            value: randNum,
-            isHeld: false,
-            id: nanoid(10)
-          }
+          createNewDie()
         )
     } 
 
@@ -25,56 +38,54 @@ function App() {
   }
 
   function rollDice() {
-    //when set state check if held, if its true dice value should remain the same
-    // setDice(allNewDice())
     setDice(prevDice => {
-      const updatedDice = prevDice.map(die => {
+      return prevDice.map(die => {
         if(die.isHeld){
           return die
         } else {
-          return { 
-            value: Math.floor(Math.random() * 6 + 1),
-            isHeld: false,
-            id: nanoid(10)
-          }
+          return createNewDie()
         }
-      })
-      return updatedDice
-    })
+      }) //end of map
+    }) //end of setDice
   }
 
   function handleHold(id) {
     setDice(prevDice => {
-      const updatedDice = prevDice.map(die => {
+      return prevDice.map(die => {
         if(die.id === id){
           return {...die, isHeld: !die.isHeld}
         } else {
           return die
         }
-      })
+      }) //end of map
+    }) //end of setDice
+  }
 
-      return updatedDice
-    })
+  function resetGame() {
+    setTenzies(false)
+    setDice(allNewDice())
   }
 
   const diceElements = dice.map(die => {
     return <Dice 
       value={die.value} 
-      handleHold={handleHold} 
+      handleHold={() => handleHold(die.id)} 
       isHeld={die.isHeld}
-      id={die.id} 
       key={die.id} 
     />
   })
 
-  console.log(diceElements)
-
   return (
-    <main className='main--container'>
+    <main className="main--container">
+      {tenzies && <Confetti />}
+      <div className="game_info--container">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      </div>
       <div className='dice--container'>
         {...diceElements}
       </div>
-      <button className='btn' onClick={rollDice}>Roll Dice</button>
+      <button className='btn' onClick={tenzies ? resetGame : rollDice} >{tenzies ? 'New Game' : 'Roll Dice'}</button>
     </main>
   )
 }
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 
-function Dice({ value, handleHold, isHeld, id }) {
+function Dice({ value, handleHold, isHeld }) {
 
     const styles = {
         backgroundColor: isHeld ? '#59E391' : '#ffffff'
     }
 
     return (
-      <div className="dice--component" style={styles} onClick={() => handleHold(id)}> 
+      <div className="dice--component" style={styles} onClick={handleHold}> 
         <h2>{value}</h2>
       </div>
     )
